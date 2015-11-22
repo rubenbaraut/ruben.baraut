d0dfb9c606012c83e90a1b88139657855b1f183c
Ruben <noemrallis@gmail.com>
2015-11-22 20:48:17 +0100

    camel case

---
+++ src/AppBundle/Resources/views/templateFiles/recent_works.php
@@ -0,0 +1,129 @@
+		<div class="row section recentworks topspace">
+			
+			<h2 class="section-title"><span>Feines i col·laboracions recents</span></h2>
+			<br />
+			<br />
+			<div class="thumbnails recentworks row">
+				
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/wavecontrol_web.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Web Wavecontrol	</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">* Wavecontrol S.L </a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+				
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/dichaea.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Dichaea</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">Disseny Txema Morales</a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/fesadent.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Fesadent</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">Disseny Txema Morales</a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/nexe.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Nexe</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">* Fados produccions </a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/lakeka.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">La Keka</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">Disseny Esther Ferrutz</a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/genoxage.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Genoxage</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">Disseny Esther Ferrutz</a> | <a href="">* Fados produccions </a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/challenge.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Challenge Barcelona</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">Disseny Esther Ferrutz</a> | <a href="">* Fados produccions </a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/fito.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">Fitohobby</span>
+					</a>
+					<span class="details"><a href="">Symfony</a> | <a href="">HTML5</a> | <a href="">Disseny Esther Ferrutz</a> | <a href="">* Fados produccions </a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+					<a class="thumbnail" href="sidebar-right.html">
+						<span class="img">
+							<img src="assets/images/2345.jpg" alt="">
+							<span class="cover"><span class="more">Visitar Web &rarr;</span></span>
+						</span>
+						<span class="title">2345 Arquitectes</span>
+					</a>
+					<span class="details"><a href="">Cake PHP</a> | <a href="">HTML5</a> | <a href="">Disseny Esther Ferrutz</a></span>
+					<h4></h4>
+					<p></p>
+				</div>
+
+				
+
+			</div>
+
+		</div> <!-- /section -->
